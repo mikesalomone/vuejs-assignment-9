415b5b1f77f197126ed3495b636d0ca55c1cd715
Mike Salomone <mike@123.com>
2017-09-15 18:02:09 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="container">
+        <form v-if="!isSubmitted">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <!-- Exercise 1 -->
+                        <!-- Create a Signup Form where you retrieve the following Information -->
+                        <!-- Full Name (First Name + Last Name) -->
+                        <!-- Mail -->
+                        <!-- Password -->
+                        <!-- Store Data? Yes/No -->
+
+                    <!-- Exercise 2 -->
+                    <!-- Only display the Form if it has NOT been submitted -->
+                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+                    <!-- Exercise 3 -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
+                    
+                    <app-full-name v-model="fullName"></app-full-name>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input 
+                                type="text"
+                                id="email"
+                                class="form-control"
+                                v-model="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input 
+                                type="password"
+                                id="password"
+                                class="form-control"
+                                v-model="password">
+                    </div>
+                    <h5>Store Data?</h5>
+                    <div class="form-group">
+                        <label for="yes">
+                            <input 
+                                type="radio"
+                                id="yes"
+                                value="Yes"
+                                v-model="storeData"> Yes
+                        </label>
+                        <label for="no">
+                            <input 
+                                    type="radio"
+                                    id="no"
+                                    value="No"
+                                    v-model="storeData"> No
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button 
+                            type="submit"
+                            class="btn btn-primary"
+                            @click.prevent="submitted">
+                            Submit
+                    </button>
+                </div>
+            </div>  
+        </form>
+        <hr>
+        <div class="row" v-if="isSubmitted">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Full Name: {{ fullName }}</p>
+                        <p>Mail: {{ email }}</p>
+                        <p>Password: {{ password }}</p>
+                        <p>Store in Database?: {{ storeData }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import FullName from './FullName.vue';
+    export default {
+        data() {
+            return {
+                fullName: 'Mike Salomone',
+                email: '',
+                password: '',
+                storeData: 'Yes',
+                isSubmitted: false
+            }
+        },
+        methods: {
+            submitted() {
+                this.isSubmitted = true;
+            }
+        },
+        components: {
+            appFullName: FullName
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/FullName.vue
@@ -0,0 +1,51 @@
+<template>
+	<div>
+		<div class="form-group">
+	        <label for="firstName">First Name</label>
+	        <input 
+	                type="text"
+	                id="firstName"
+	                class="form-control"
+	                :value="firstName"
+	                @input="emitFullName">
+	    </div>
+	    <div class="form-group">
+	        <label for="lastName">Last Name</label>
+	        <input 
+	                type="text"
+	                id="lastName"
+	                class="form-control"
+	                :value="lastName"
+	                @input="emitFullName">
+	    </div>
+    </div>
+</template>
+
+<script>
+	export default {
+		props: ['value'],
+		methods: {
+			emitFullName(event) {
+				let name = '';
+				if (event.target.id == 'firstName') {
+					name = event.target.value + ' ' + this.lastName;
+				} else {
+					name = this.firstName + ' ' + event.target.value;
+				}
+				this.$emit('input', name);
+			}
+		},
+		computed: {
+			firstName() {
+				return this.value.split(" ")[0];
+			},
+			lastName() {
+				return this.value.split(" ")[1];
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
